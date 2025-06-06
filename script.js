633aa3dff6174146caf6a5f83641821da11cab2e
devashree75 <devadevashree75@gmail.com>
2025-06-06 15:05:46 +0530

    first command

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('hello from script');
+console.log(5=='5')
+console.log(5==='5')
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+    function greet(name,age){
+        return'hello '+name +'is '+age+' old'
+    }
+    console.log(greet('ronaldo ',40 ));
+    function add(a,b){
+        return'the sum of ' +a +' and ' +b +' is '+a+b
+    }
+    console.log(add(5,2))
+    const greet=(name,age)=>{
+
+    }
+
